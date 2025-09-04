292ea0a8c2834052880e850c3be662aa5438a702
zachmann <gabriel.zachmann@kit.edu>
2025-09-04 15:31:24 +0200

    add SnakeToCamel to stringutils with initialisms support

--- stringutils/strings.go
+++ stringutils/strings.go
@@ -1,6 +1,74 @@
 package utils
 
+import (
+	"strings"
+	"unicode"
+)
+
 // EqualIfSet checks if two strings are equal if they are both not empty
 func EqualIfSet(a, b string) bool {
 	return a == "" || b == "" || a == b
+}
+
+var commonInitialisms = map[string]struct{}{
+	"API":   {},
+	"ASCII": {},
+	"CPU":   {},
+	"CSS":   {},
+	"DNS":   {},
+	"EOF":   {},
+	"GUID":  {},
+	"HTML":  {},
+	"HTTP":  {},
+	"HTTPS": {},
+	"ID":    {},
+	"IP":    {},
+	"JSON":  {},
+	"LHS":   {},
+	"QPS":   {},
+	"RAM":   {},
+	"RHS":   {},
+	"RPC":   {},
+	"SLA":   {},
+	"SMTP":  {},
+	"SQL":   {},
+	"SSH":   {},
+	"TCP":   {},
+	"TLS":   {},
+	"TTL":   {},
+	"UDP":   {},
+	"UI":    {},
+	"UID":   {},
+	"UUID":  {},
+	"URI":   {},
+	"URL":   {},
+	"UTF8":  {},
+	"VM":    {},
+	"XML":   {},
+	"XSRF":  {},
+	"XSS":   {},
+}
+
+func SnakeToCamel(s string) string {
+	if s == "" {
+		return ""
+	}
+	parts := strings.FieldsFunc(s, func(r rune) bool { return r == '_' })
+
+	var b strings.Builder
+	for _, p := range parts {
+		if p == "" {
+			continue
+		}
+		lower := strings.ToLower(p)
+		upper := strings.ToUpper(lower)
+		if _, ok := commonInitialisms[upper]; ok {
+			b.WriteString(upper)
+			continue
+		}
+		runes := []rune(lower)
+		runes[0] = unicode.ToUpper(runes[0])
+		b.WriteString(string(runes))
+	}
+	return b.String()
 }
